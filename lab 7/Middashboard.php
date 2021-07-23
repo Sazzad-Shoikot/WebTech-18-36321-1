e34d8d9f3584b7a2b216211e27d4bd2c59a0f3d4
Shoikot <39308140+Sazzad-Shoikot@users.noreply.github.com>
2021-07-23 19:33:05 +0600

    Add files via upload

---
+++ lab 7/Middashboard.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+<fieldset style="width: 200px; ">
+<pre><img src="midprojectlogo.jpg" alt="student" width="50" height="60">                                                    Logged in as <a href="">Shusmita Islam</a>|<a href="">Logout</a></pre>               
+<hr style="border: 0.1px solid;">
+<div style="background:white; height: 300px; border-left: 2.0px black solid; left:200px; position: absolute;">
+<pre>                                                                     <h2>   WELCOME Shusmita Islam</h2></pre>
+</div>
+<pre><b>Student Panel</b><br><hr style="border: 0.1px solid;">
+ <a href=""><u>Dashboard</u></a>
+ <a href=""><u>View subject</u></a>
+ <a href=""><u>View result</u></a>
+ <a href=""><u>View notes</u></a>
+ <a href=""><u>View notice</u></a>
+ <a href=""><u>Upload files</u></a>
+ <a href=""><u>Borrow books</u></a></pre>
+<pre><b>Personal</b><br><hr style="border: 0.1px solid;">
+ <a href=""><u>Edit Profile</u></a>                 
+ <a href=""><u>Change Profile Picture</u></a>
+ <a href=""><u>Change Password</u></a>
+ <a href=""><u>Logout</u></a>
+
+</pre>
+</fieldset>
+</form>
+</body>
+</html>
